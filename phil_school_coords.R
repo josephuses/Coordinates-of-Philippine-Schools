0d4bf0094379169a4013aead240b98c9a652fd04
josephuses <josephuses@gmail.com>
2018-04-01 16:42:46 +0800

    added R script file

---
+++ phil_school_coords.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(readit)
+library(ggmap)
+
+# import the file
+df <- readit('DepEDSchools with Missing GPS Data - Sheet1.csv')
+# create a new column that contains the school name and address
+df <- df %>% mutate(address = paste(school_name, municipality, province, sep = ', '))
+# do some cleaning: 
+# NHS -> National High School
+# ES -> Elementary School
+# HS -> High School
+df$address <- str_replace(df$address, 'NHS', 'National High School')
+df$address <- str_replace(df$address, 'ES', 'Elementary School')
+df$address <- str_replace(df$address, ' HS', ' High School')
+
+# Put df$address into the object place
+place <- df$address
+
+# Get the geo coordinates, limited to 2500 queries (rows) per day
+codes <- geocode(place[1:2500])
+
+names(df)
+names(codes)
+
+# replace the NA's with the matches in codes
+df[1:2500,'latitude'] <- codes[,'lat']
+df[1:2500,'longitude'] <- codes[,'lon']
+df %>% select(latitude, longitude)
+
+# write csv to file noLongLat.csv
+write_csv(df, 'noLongLat.csv')
+
+# get all rows with the remaining missing longitude and latitude
+df2 <- df %>% filter(is.na(latitude))
+# write csv to file updatedNoLongLat.csv
+write_csv(df2, 'updatedNoLongLat.csv')
